6bc32c48bcb5d2bc3bdb80392cadcac334d7d3ab
lenatnog <helena_1306@hotmail.com>
2025-04-02 10:50:22 -0300

    Added a footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        Created by Helena Nogueira, open-sourced on{" "}
+        <a
+          href="https://github.com/lenatnog/real-react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://real-react-weather-app.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
-
-
