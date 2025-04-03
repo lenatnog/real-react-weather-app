72b4408f48381bca8a2a71ae044bf36b610d8078
lenatnog <helena_1306@hotmail.com>
2025-04-03 18:54:47 -0300

    Added basic HTML

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import Weather from "./Weather"
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h1>Weather App</h1>        
+      <div className="container">       
+        <Weather />        
         <footer>
           Created by Helena Nogueira, open-sourced on{" "}
           <a
