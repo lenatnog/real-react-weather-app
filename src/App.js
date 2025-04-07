9c2ed8a216beacdccc63996e6f64225282ec22f2
lenatnog <helena_1306@hotmail.com>
2025-04-07 17:43:42 -0300

    Added API Integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Weather from "./Weather"
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">       
-        <Weather />        
+      <div className="container">
+        <Weather defaultCity="São Paulo" />
         <footer>
           Created by Helena Nogueira, open-sourced on{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,44 +1,79 @@
-import React from "react";
-import "./Weather.css";
+  import React, { useState } from "react";
+  import axios from "axios";
+  import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
+  export default function Weather(props) {
+
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    
+    function handleResponse(response){
+     setWeatherData({
+       ready: true,
+       temperature: response.data.main.temp,
+       humidity: response.data.main.humidity,
+       date: "Wednesday 07:00",
+       description: response.data.weather[0].description,
+       iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+       wind: response.data.wind.speed,
+       city: response.data.name
+     });
+    }
+
+    if(weatherData.ready){
+      return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+           <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6 image-position">
+            <img
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
             />
+              <span className="temperature"> {Math.round(weatherData.temperature)}</span>
+              <span className="unit">°C</span>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+               <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Wednesday 7:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="Mostly Cloudy"
-          ></img>
-          22°C
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Rain: 0%</li>
-            <li>Humidity: 70%</li>
-            <li>Wind: 19 km/h</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+    } else {
+      const apiKey = "3499ef150985eccadd080ff408a018df";
+    let city = "London"
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(handleResponse)
+
+      return(
+        "loading..."
+      );
+    }
+
+    
+  }
